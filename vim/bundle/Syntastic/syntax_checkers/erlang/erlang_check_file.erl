ff996dab69b55b3219aa62b2bbe6cd36c23c0597
Hongzhang Liu <hongzhang.liu@gmail.com>
2013-10-19 22:36:30 +0800

    modified as need

--- vim/bundle/Syntastic/syntax_checkers/erlang/erlang_check_file.erl
+++ vim/bundle/Syntastic/syntax_checkers/erlang/erlang_check_file.erl
@@ -9,7 +9,6 @@ main([FileName | ["-rebar" | [Path | LibDirs]]]) ->
     {ok, L} = file:consult(Path),
     P = dict:from_list(L),
     Root = filename:dirname(Path),
-
     Lib1 = case dict:find(lib_dirs, P) of
              {ok, X} -> lists:map(fun(Sub) -> Root ++ "/" ++ Sub end, X);
              _ -> []
@@ -28,7 +27,6 @@ main([FileName | ["-rebar" | [Path | LibDirs]]]) ->
            end,
 
     LibDirs1 = LibDirs ++ Lib1 ++ Lib2,
-    %io:format("~p~n", [LibDirs1]),
     compile(FileName, LibDirs1);
 
 main([FileName | LibDirs]) ->
@@ -36,6 +34,9 @@ main([FileName | LibDirs]) ->
 
 compile(FileName, LibDirs) ->
     Root = get_root(filename:dirname(FileName)),
+    {ok, F} = file:open("/Users/liuhongzhang/erl_check.txt", [write]),
+    io:format(F, "FileName = ~p~nRoot = ~p~n", [FileName, Root]),
+    file:close(F),
     ok = code:add_pathsa(LibDirs),
     compile:file(FileName, [warn_obsolete_guard,
                             warn_unused_import,
@@ -51,13 +52,19 @@ compile(FileName, LibDirs) ->
 
 get_root(Dir) ->
     Path = filename:split(filename:absname(Dir)),
-    filename:join(get_root(lists:reverse(Path), Path)).
+    get_root(lists:reverse(Path), Dir).
 
-get_root([], Path) ->
-    Path;
-get_root(["src" | Tail], _Path) ->
-    lists:reverse(Tail);
-get_root(["test" | Tail], _Path) ->
-    lists:reverse(Tail);
-get_root([_ | Tail], Path) ->
-    get_root(Tail, Path).
+% we define the root as where Emakefile sits
+has_emakefile(Files) ->
+    lists:member("Emakefile", Files).
+
+get_root([], Dir) ->
+    Dir;
+get_root(L = [_ | T], Dir) ->
+    P = filename:join(lists:reverse(L)),
+    {ok, Files}  = file:list_dir(P),
+    case has_emakefile(Files) of
+        true -> P;
+        _ -> get_root(T, Dir)
+    end.
+
